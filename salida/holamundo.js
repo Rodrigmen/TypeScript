fc675edb6d2269c04b397cc6b9143cb16db3d2c2
Rodrigo Robles <roblesminambresrodrigo@gmail.com>
2021-10-05 19:57:23 +0200

    Creación de .json que indica donde crear los arvhivos .js tras la correcta compilacion de los archivos .ts

--- salida/holamundo.js
+++ salida/holamundo.js
@@ -1,6 +1,7 @@
 function calcula(a, b) {
     return a + b;
 }
+// CTRL+SHIFT+B = COMPILACION ----- CTRL+F5 = EJECUCIÓN
 var message = "Hola mundo";
 console.log(message);
 console.log(calcula(3, 5));
--- src/holamundo.ts
+++ src/holamundo.ts
@@ -1,7 +1,7 @@
 function calcula(a: number, b: number) {
     return a + b;
 }
-
+// CTRL+SHIFT+B = COMPILACION ----- CTRL+F5 = EJECUCIÓN
 let message: string = "Hola mundo";
 console.log(message);
 console.log(calcula(3, 5));
